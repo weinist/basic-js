5e6fbfa189e92f8f19050d3b14848b420ff78d89
Daria Trubnikova <daria.trubnikova@gmail.com>
2021-03-25 21:33:44 +0300

    fix: add extended-repeater decision

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,60 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function repeater(str, options) {
+
+  let res = [];
+  let result = [];
+  if(!options) {
+    return false;
+  }
+
+  if(!String(str)) {
+    str = String(str);
+  }
+
+  if(str == null) {
+    str = 'null';
+  }
+
+  if(!String(options.addition)) {
+    options.addition = String(options.addition);
+  }
+
+  if(options.addition === null) {
+    options.addition = 'null';
+  }
+
+  if(!options.separator) {
+    options.separator = '+';
+  }
   
+  if(!String(options.separator)) {
+    options.separator = String(options.separator);
+  }
+
+  if(!options.additionSeparator) {
+    options.additionSeparator = '|';
+  }
+
+  for(let i = 0; i < options.additionRepeatTimes - 1; i++) {
+    res.push(options.addition);
+    res.push(options.additionSeparator);
+  }
+    res.push(options.addition);
+    res.push(str);
+    str = res.reverse().join('');
+  
+  for(let k = 0; k < options.repeatTimes - 1; k++) {
+    result.push(str);
+    result.push(options.separator);
+  }
+    result.push(str);
+    return result.join('');
+  //throw new CustomError('Not implemented');
+  // remove line with error and write your code here
+
+
+
+
+
+};
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,8 +1,23 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new CustomError('Not implemented');
+  calculateDepth(arr) {
+    if(!Array.isArray(arr)) {
+      throw Error('Error');
+    } else if(arr.length === 0) {
+      return 1;
+    } else {
+      let result = 0;
+      const res = arr.toString().split('');
+      console.log(res);
+      for(let i = 0; i < res.length; i++) {
+        if(res[i] === ']') {
+          result++;
+        }
+      }
+    }
+    return result;
+    //throw new CustomError('Not implemented');
     // remove line with error and write your code here
   }
 };
